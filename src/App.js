66d3800e6ff3b4bbe7789e1d24f3ea7e5d820219
Mark Rodighiero <markatango@gmail.com>
2021-04-11 06:51:57 -0700

    menu-item styling with hover added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import './App.css';
-import HomePage from './components/homepage.component';
+import HomePage from './pages/homepage/homepage.component';
 
 import './App.css';
 
 function App() {
   return (
     <div>
-    <HomePage></HomePage>
+      <HomePage/>
     </div>
   );
 }
