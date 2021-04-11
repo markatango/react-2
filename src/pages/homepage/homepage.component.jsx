d22dabd7a12da837a0c9ec7115eed201c89cc9a3
Mark Rodighiero <markatango@gmail.com>
2021-04-11 08:28:37 -0700

    refactored components in homepage

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -5,7 +5,7 @@ import Directory from '../../components/directory/directory.component';
 
 const HomePage  = () => ( 
     <div className='homepage'>
-        <Directory className='directory-menu'></Directory>
+        <Directory />
     </div>
 );
 
