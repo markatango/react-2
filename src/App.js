ca9b22f1e7fcda0014c5dddec0e098c80ccc8546
Mark Rodighiero <markatango@gmail.com>
2021-04-24 05:35:31 -0700

    added header component with logo

--- src/App.js
+++ src/App.js
@@ -3,15 +3,20 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import { Switch, Route } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 //import RouteDemo from './pages/routedemo/routedemo.component';
 
 
 function App() {
   return (
+  <div>
+    <Header />
     <Switch>
       <Route exact path='/' component={ HomePage } />
       <Route path='/shop' component={ ShopPage } />
     </Switch>
+  </div>
+    
   );
 }
 
