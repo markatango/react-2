82dbee4ca514204364162cea7938a8e09147fd80
Mark Rodighiero <markatango@gmail.com>
2021-04-23 21:08:42 -0700

    added collection-items and styling

--- src/App.js
+++ src/App.js
@@ -2,23 +2,15 @@ import React from 'react'
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import { Switch, Route } from 'react-router-dom';
-//import ShopPage from './pages/shop/shop.component';
+import ShopPage from './pages/shop/shop.component';
 //import RouteDemo from './pages/routedemo/routedemo.component';
-
-const ShopHats = (props) => {
-  console.log(props)
-  return(
-  <div>
-    <h1>Shop for hats</h1>
-  </div>
-)}
 
 
 function App() {
   return (
     <Switch>
       <Route exact path='/' component={ HomePage } />
-      <Route path='/shop/hats' component={ ShopHats } />
+      <Route path='/shop' component={ ShopPage } />
     </Switch>
   );
 }
